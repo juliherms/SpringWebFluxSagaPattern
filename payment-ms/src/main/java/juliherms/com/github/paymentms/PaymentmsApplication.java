7bb37891da6ad93a220b65662ac03b93032a88e9
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-09-12 11:47:58 -0300

    create payment microservices

---
+++ payment-ms/src/main/java/juliherms/com/github/paymentms/PaymentmsApplication.java
@@ -0,0 +1,13 @@
+package juliherms.com.github.paymentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentmsApplication.class, args);
+	}
+
+}
